f83455697dfbbb422f73f8b1f1ab33f1c955b42e
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-10-21 16:08:09 +0300

    enhancement
    1. changed real slot names mentionung like "Mo Mummy" to more generic  keywords like "/Slot Name/"
    2. two "win the 2x|3x" bug fixed
    3. lack of sale word in sale asset name bug fixed
    4. assets order changed

--- assetRequestTextGeneration.py
+++ assetRequestTextGeneration.py
@@ -9,10 +9,10 @@ def ask(thing):
 
 def add_multipliers(slot_name_no_symbols, multipliers):
 
-    if multipliers[-1] == '': # in case someone can enter extra space in the end of multipliers input
+    if multipliers[-1] == '':  # in case someone can enter extra space in the end of multipliers input
         multipliers = multipliers[0:-1]
 
-    sting1 = slot_name_no_symbols
+    sting1 = slot_name_no_symbols + 'Sale'
     string2 = ''
 
     for i in multipliers:
@@ -39,11 +39,11 @@ def replacer(new_text, old_text, doc_content):
 
 def replacer_jp_name(new_text, old_text, new_slot_name_normal, doc_content):
     if new_text == 'jp':
-        doc_content = doc_content.replace(old_text, f"Win 2x | 3x the Jackpot! (there aren't multiple jackpots in {new_slot_name_normal})")
+        doc_content = doc_content.replace(old_text, f"Jackpot! (there aren't multiple jackpots in {new_slot_name_normal})")
     if new_text == '':
-        doc_content = doc_content.replace(old_text, f"Win 2x | 3x the TBD Jackpot! (there [aren't/are] multiple jackpots in {new_slot_name_normal})")
+        doc_content = doc_content.replace(old_text, f"TBD Jackpot! (there [aren't/are] multiple jackpots in {new_slot_name_normal})")
     else:
-        doc_content = doc_content.replace(old_text, f"Win 2x | 3x the {new_text}! (there are multiple jackpots in {new_slot_name_normal})")
+        doc_content = doc_content.replace(old_text, f"{new_text}! (there are multiple jackpots in {new_slot_name_normal})")
     return doc_content
 
 
@@ -56,56 +56,71 @@ def handler(doc_file, new_name, buyInIds_BFC, buyInIds_JMS, theme_name):
     file = open(file_path, 'r')
     file_content = file.read()
 
-    file_content = replacer(new_name_no_space, 'MoMummy', file_content)
-    file_content = replacer(new_name, 'Mo Mummy', file_content)
-    file_content = replacer(theme_name, 'momummy', file_content)
-    file_content = replacer(add_multipliers(new_name_no_space, saleMultipliersBFC), 'NameWithMultipliersBFC', file_content)
-    file_content = replacer(add_multipliers(new_name_no_space, saleMultipliersJMS), 'NameWithMultipliersJMS', file_content)
-    file_content = replacer_jp_name(largest_jp_name, 'largest_jp_name_here', newSlotNameNormal, file_content)
+    file_content = replacer(new_name_no_space, '/SlotName/', file_content)
+    file_content = replacer(new_name, '/Slot Name/', file_content)
+    file_content = replacer(theme_name, '/themename/', file_content)
+    file_content = replacer(add_multipliers(new_name_no_space, saleMultipliersBFC), '/NameWithMultipliersBFC/', file_content)
+    file_content = replacer(add_multipliers(new_name_no_space, saleMultipliersJMS), '/NameWithMultipliersJMS/', file_content)
+    file_content = replacer_jp_name(largest_jp_name, '/largestJPNameHere/', newSlotNameNormal, file_content)
 
     try:
-        file_content = replacer(str(buyInIds_BFC[0]), 'NonVIP_BuyInIDsBFC', file_content)
+        file_content = replacer(str(buyInIds_BFC[0]), '/NonVIPBuyInIDsBFC/', file_content)
     except IndexError:
-        file_content = replacer('TBD', 'NonVIP_BuyInIDsBFC', file_content)
+        file_content = replacer('TBD', '/NonVIPBuyInIDsBFC/', file_content)
 
     try:
-        file_content = replacer(str(buyInIds_BFC[1]), 'VIP_BuyInIDsBFC', file_content)
+        file_content = replacer(str(buyInIds_BFC[1]), '/VIPBuyInIDsBFC/', file_content)
     except IndexError:
-        file_content = replacer('TBD', 'VIP_BuyInIDsBFC', file_content)
+        file_content = replacer('TBD', '/VIPBuyInIDsBFC/', file_content)
 
     try:
-        file_content = replacer(str(buyInIds_BFC[2]), 'HR_BuyInIDsBFC', file_content)
+        file_content = replacer(str(buyInIds_BFC[2]), '/HRBuyInIDsBFC/', file_content)
     except IndexError:
-        file_content = replacer('TBD', 'HR_BuyInIDsBFC', file_content)
+        file_content = replacer('TBD', '/HRBuyInIDsBFC/', file_content)
 
     try:
-        file_content = replacer(str(buyInIds_JMS[0]), 'NonVIP_BuyInIDsJMS', file_content)
+        file_content = replacer(str(buyInIds_JMS[0]), '/NonVIPBuyInIDsJMS/', file_content)
     except IndexError:
-        file_content = replacer('TBD', 'NonVIP_BuyInIDsJMS', file_content)
+        file_content = replacer('TBD', '/NonVIPBuyInIDsJMS/', file_content)
 
     try:
-        file_content = replacer(str(buyInIds_JMS[1]), 'VIP_BuyInIDsJMS', file_content)
+        file_content = replacer(str(buyInIds_JMS[1]), '/VIPBuyInIDsJMS/', file_content)
     except IndexError:
-        file_content = replacer('TBD', 'VIP_BuyInIDsJMS', file_content)
+        file_content = replacer('TBD', '/VIPBuyInIDsJMS/', file_content)
 
     pyperclip.copy(file_content)
     print(file_content)
 # </editor-fold>
 
 # <editor-fold desc="ask input part">
-newSlotNameNormal = ask('SLOT NAME')
-saleMultipliersBFC = ask('BFC SALE MULTIPLIERS, separated by space, eg "5.5 1.25"').split(' ')
-saleMultipliersJMS = ask('JMS SALE MULTIPLIERS, separated by space, eg "5.5 1.25"').split(' ')
-buyInId_BFC = ask('BFC buy-in id nonVIP, VIP and HR IDs respectively, separated by space, eg "111 222 333", ').split(' ')
-buyInId_JMS = ask('JMS buy-in id nonVIP, VIP IDs respectively, separated by space, eg "111 222", ').split(' ')
-largest_jp_name = ask('the LARGEST JP NAME if there are few, "jp" if there is only 1, ')
-theme = ask('SLOT THEME')
+# newSlotNameNormal = ask('SLOT NAME')
+newSlotNameNormal = 'Big Fat Dragon'
+
+# saleMultipliersBFC = ask('BFC SALE MULTIPLIERS, separated by space, eg "5.5 1.25"').split(' ')
+saleMultipliersBFC = '5.25 5.5 6 6.5'.split(' ')
+
+#saleMultipliersJMS = ask('JMS SALE MULTIPLIERS, separated by space, eg "5.5 1.25"').split(' ')
+saleMultipliersJMS = '1.25 1.5 2'.split(' ')
+
+# buyInId_BFC = ask('BFC buy-in id nonVIP, VIP and HR IDs respectively, separated by space, eg "111 222 333", ').split(' ')
+buyInId_BFC = '111 222 333'.split(' ')
+
+# buyInId_JMS = ask('JMS buy-in id nonVIP, VIP IDs respectively, separated by space, eg "111 222", ').split(' ')
+buyInId_JMS = '4444 5555'.split(' ')
+
+# largest_jp_name = ask('the LARGEST JP NAME if there are few, "jp" if there is only 1, ')
+largest_jp_name = 'Grand'
+# largest_jp_name = ''
+# largest_jp_name = 'jp'
+
+# theme = ask('SLOT THEME')
+theme = 'fatdragon'
 # </editor-fold>
 
 while True:
     doc = pyinputplus.inputMenu(['teaser+launch',
-                                 'jpSurge',
                                  'sales',
+                                 'jpSurge',
                                  'ejp',
                                  'offer'], '\n\nenter number to copy to clipboard:\n', numbered=True)
 
